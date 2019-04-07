014ef889b8e3201e8d5075465dd94c495e9248cc
Chandrajeet Choudhary <chandrajeet.c16@iiits.in>
2019-04-07 12:18:22 +0530

    final

--- lib/load_repos.dart
+++ lib/load_repos.dart
@@ -16,9 +16,9 @@ Future<dynamic> getRepos() async {
 
 Widget container(Widget child) {
   return Container(
-    height: 20.0,
-    width: 20.0,
-    margin: EdgeInsets.all(10.0),
+    height: 20,
+    width: 20,
+    margin: EdgeInsets.all(10),
     child: child,
   );
 }
@@ -28,24 +28,23 @@ List<Widget> allrepos(repo_list) {
   for (dynamic repo in repo_list) {
     allrepos_widget.add(
       Container(
-        margin: EdgeInsets.only(bottom: 5.0, left: 5.0, right: 5.0),
-        padding: EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
+        margin: EdgeInsets.only(bottom: 5, left: 5, right: 5),
+        padding: EdgeInsets.only(top: 8, left: 8, right: 8),
         decoration: BoxDecoration(
           color: Colors.white,
           border: Border.all(color: Colors.white),
         ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Container(
-              padding: EdgeInsets.all(10.0),
+              padding: EdgeInsets.all(10),
               child: GestureDetector(
                 child: Text(
                   repo['name'].toString(),
                   style: TextStyle(
-                      fontSize: 15.0,
+                      fontSize: 20,
                       fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.italic,
                       color: Colors.blue),
                 ),
                 onTap: () async {
@@ -54,38 +53,41 @@ List<Widget> allrepos(repo_list) {
                 },
               ),
             ),
-            Text(repo['description'].toString()),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: Text(repo['description'].toString()),
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
                 Container(
-                  height: 20.0,
-                  width: 20.0,
-                  margin: EdgeInsets.all(10.0),
+                  height: 20,
+                  width: 20,
+                  margin: EdgeInsets.all(10),
                   decoration: BoxDecoration(
                       color: Color(0xFF487BEA),
                       border: Border.all(color: Colors.white),
-                      borderRadius: BorderRadius.all(Radius.circular(50.0))),
+                      borderRadius: BorderRadius.all(Radius.circular(50))),
                 ),
                 Text(repo['language'].toString()),
                 container(
                   Icon(
                     Icons.star,
-                    size: 20.0,
+                    size: 20,
                   ),
                 ),
                 Text(repo['stargazers_count'].toString()),
                 container(
                   Icon(
                     Icons.merge_type,
-                    size: 15.0,
+                    size: 15,
                   ),
                 ),
                 Text(repo['forks_count'].toString()),
                 Container(
-                  margin: EdgeInsets.only(left: 40.0, right: 10.0),
-                  width: 20.0,
-                  height: 20.0,
+                  margin: EdgeInsets.only(left: 20, right: 5),
+                  width: 20,
+                  height: 20,
                   decoration: BoxDecoration(
                     color: Colors.white,
                     border: Border.all(color: Colors.white),
@@ -95,8 +97,6 @@ List<Widget> allrepos(repo_list) {
                 GestureDetector(
                   child: Text(
                     repo['owner']['login'].toString(),
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
                   ),
                   onTap: () async {
                     var url = repo['html_url'].toString();
@@ -130,21 +130,23 @@ class _ShowRepoState extends State<ShowRepo> {
         title: Text("Top Github Repos"),
         backgroundColor: Color(0xFF42A5F5),
       ),
-      body: SingleChildScrollView(
-        child: FutureBuilder(
-          future: getRepos(),
-          builder: (context, snap) {
-            if (snap.hasData) {
-              return Column(
-                children: allrepos(snap.data['items']),
-              );
-            } else {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          },
-        ),
+      body: FutureBuilder(
+        future: getRepos(),
+        builder: (context, snap) {
+          if (snap.hasData) {
+            return SingleChildScrollView(
+              child: Column(
+                children: allrepos(
+                  snap.data['items'],
+                ),
+              ),
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
   }
