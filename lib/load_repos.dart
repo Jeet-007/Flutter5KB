6866db39ffb4f0b420c84b4c6436e7ae136168d2
Chandrajeet Choudhary <chandrajeet.c16@iiits.in>
2019-04-07 10:47:36 +0530

    < 5kb final changes

--- lib/load_repos.dart
+++ lib/load_repos.dart
@@ -7,33 +7,23 @@ import 'package:url_launcher/url_launcher.dart';
 import './globals.dart';
 
 Future<dynamic> getRepos() async {
-  final url = (gitRepo).toString();
+  dynamic res = await http.get((gitRepo).toString());
 
-  dynamic response = await http.get(url);
-
-  if (response.statusCode == 200) {
-    dynamic responseJson = json.decode(response.body);
-    // print(responseJson);
-    return responseJson;
-  } else {
-    throw Exception(response.body);
-  }
+  return res.statusCode == 200
+      ? json.decode(res.body)
+      : throw Exception(res.body);
 }
 
-class HexColor extends Color {
-  static int _getColorFromHex(String hexColor) {
-    hexColor = hexColor.toUpperCase().replaceAll("#", "");
-    if (hexColor.length == 6) {
-      hexColor = "FF" + hexColor;
-    }
-    return int.parse(hexColor, radix: 16);
-  }
-
-  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+Widget container(Widget child) {
+  return Container(
+    height: 20.0,
+    width: 20.0,
+    margin: EdgeInsets.all(10.0),
+    child: child,
+  );
 }
 
 List<Widget> allrepos(repo_list) {
-  // print(repo_list);
   List<Widget> allrepos_widget = List();
   for (dynamic repo in repo_list) {
     allrepos_widget.add(
@@ -41,31 +31,28 @@ List<Widget> allrepos(repo_list) {
         margin: EdgeInsets.only(bottom: 5.0, left: 5.0, right: 5.0),
         padding: EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
         decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border.all(color: Colors.white),
-            borderRadius: BorderRadius.all(Radius.circular(5.0))),
+          color: Colors.white,
+          border: Border.all(color: Colors.white),
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             Container(
               padding: EdgeInsets.all(10.0),
               child: GestureDetector(
-                  child: Text(
-                    repo['name'].toString(),
-                    style: TextStyle(
-                        fontSize: 15.0,
-                        fontWeight: FontWeight.bold,
-                        fontStyle: FontStyle.italic,
-                        color: Colors.blue),
-                  ),
-                  onTap: () async {
-                    var url = repo['html_url'].toString();
-                    if (await canLaunch(url)) {
-                      await launch(url);
-                    } else {
-                      throw 'Could not launch $url';
-                    }
-                  }),
+                child: Text(
+                  repo['name'].toString(),
+                  style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.blue),
+                ),
+                onTap: () async {
+                  var url = repo['html_url'];
+                  await canLaunch(url) ? await launch(url) : throw '$url';
+                },
+              ),
             ),
             Text(repo['description'].toString()),
             Row(
@@ -76,29 +63,20 @@ List<Widget> allrepos(repo_list) {
                   width: 20.0,
                   margin: EdgeInsets.all(10.0),
                   decoration: BoxDecoration(
-                      color: langColors[repo['language'].toString()] != null
-                          ? HexColor(langColors[repo['language'].toString()]
-                              .toString())
-                          : Colors.yellow,
+                      color: Color(0xFF487BEA),
                       border: Border.all(color: Colors.white),
                       borderRadius: BorderRadius.all(Radius.circular(50.0))),
                 ),
                 Text(repo['language'].toString()),
-                Container(
-                  height: 20.0,
-                  width: 20.0,
-                  margin: EdgeInsets.all(10.0),
-                  child: Icon(
+                container(
+                  Icon(
                     Icons.star,
                     size: 20.0,
                   ),
                 ),
-                Text(repo['watchers'].toString()),
-                Container(
-                  height: 20.0,
-                  width: 20.0,
-                  margin: EdgeInsets.all(10.0),
-                  child: Icon(
+                Text(repo['stargazers_count'].toString()),
+                container(
+                  Icon(
                     Icons.merge_type,
                     size: 15.0,
                   ),
@@ -121,12 +99,8 @@ List<Widget> allrepos(repo_list) {
                     overflow: TextOverflow.ellipsis,
                   ),
                   onTap: () async {
-                    var url = repo['owner']['html_url'].toString();
-                    if (await canLaunch(url)) {
-                      await launch(url);
-                    } else {
-                      throw 'Could not launch $url';
-                    }
+                    var url = repo['html_url'].toString();
+                    await canLaunch(url) ? await launch(url) : throw '$url';
                   },
                 ),
               ],
@@ -153,53 +127,23 @@ class _ShowRepoState extends State<ShowRepo> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Trending Github Repos"),
+        title: Text("Top Github Repos"),
         backgroundColor: Color(0xFF42A5F5),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  "Language       ",
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                DropdownButton(
-                  items: [
-                    DropdownMenuItem(
-                      child: Text("First Option"),
-                    ),
-                    DropdownMenuItem(
-                      child: Text("Second Option"),
-                    ),
-                    DropdownMenuItem(
-                      child: Text("Third Option"),
-                    ),
-                  ],
-                  onChanged: (value) {},
-                ),
-              ],
-            ),
-            FutureBuilder(
-              future: getRepos(),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return Column(
-                    children: allrepos(snapshot.data['items']),
-                  );
-                } else {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-              },
-            ),
-          ],
+        child: FutureBuilder(
+          future: getRepos(),
+          builder: (context, snap) {
+            if (snap.hasData) {
+              return Column(
+                children: allrepos(snap.data['items']),
+              );
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
         ),
       ),
     );
